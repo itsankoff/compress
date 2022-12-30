291e23943e00c106937aae5691da60f74a388d54
Ivaylo Tsankov <itsankoff@gmail.com>
2022-12-30 16:38:10 +0200

    * Init usage of dectofrac package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/itsankoff/compress
 
 go 1.19
+
+require github.com/av-elier/go-decimal-to-rational v0.0.0-20191127152832-89e6aad02ecf // indirect
--- ratio/main.go
+++ ratio/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	dectofrac "github.com/av-elier/go-decimal-to-rational"
+)
 
 func main() {
 	fmt.Println("Ratio compression placeholder")
+	fmt.Println(dectofrac.NewRatP(0.6666, 0.01).String())
 }
